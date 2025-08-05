5a96495f86e4a1e8f038d81f83f315cbe5a1228b
Harshvardhan <harshvardhan.@Harshvardhans-MacBook-Air.local>
2025-08-05 15:52:47 +0530

    Added Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://your-project.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'your-anon-key';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://rbnwkabdolxhfyouzpeg.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'youreyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJibndrYWJkb2x4aGZ5b3V6cGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzODczNDksImV4cCI6MjA2OTk2MzM0OX0.LmIsaR7LAsuzyL9VuUWORA9QuuOM5ufiLNtS3oj1KEI-anon-key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
